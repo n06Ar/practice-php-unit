3040ca44c8ad4be60b4fdba8d5740dfd90d91b4f
N06AR <nobht.arai@gmail.com>
2020-04-13 19:50:56 +0900

    add comment

--- app/src/DummyItem.php
+++ app/src/DummyItem.php
@@ -5,11 +5,13 @@ namespace App;
 interface DummyItem
 {
     /**
+     * DummyItemの名前を取得
      * @return string
      */
     public function getName():string;
 
     /**
+     * DummyItemの名前を取得
      * @return string
      */
     public function getId():string;
--- app/src/index.php
+++ app/src/index.php
@@ -1,11 +1,12 @@
 <?php
 
 require_once( __DIR__.'/CsvRelation.php');
-
 use App\CsvRelation;
 
+// CSVのファイルパス
 $filepath = __DIR__ . '/../../Public/Test.csv';
 
+// CSVに出力する
 $array = [
     ['1',2,3.4,5],
     ['1','2',3.4,5],
